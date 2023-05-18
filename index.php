fedae88b184d6da392bcaa4135c2dbb5ebec6a65
moathmajed <94411785+moathmajed@users.noreply.github.com>
2023-05-18 19:16:59 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php include "header.php"?>
+
+<!-- Start Hero Slider -->
+<section class="hero">
+    <div class="swiper-container hero-slider">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+            <img src="./img/hero-img.png" alt="">
+            <div class="overlay-color"></div>
+            <div class="overlay-text">
+                <h2>
+                Industry Leading Audio-Video Solutions.
+                </h2>
+                <p>
+                In a laoreet purus. Integer turpis quam, laoreet id orci nec, ultrices lacinia nunc. Aliquam erat volutpat. Curabitur fringilla in purus eget egestas. Etiam quis.
+                </p>
+                <a class="btn btn-white-outline" href="#">
+                    learn more
+                </a>
+            </div>
+        </div>
+        <div class="swiper-slide">
+            <img src="./img/hero-img.png" alt="">
+            <div class="overlay-color"></div>
+            <div class="overlay-text">
+                <h2>
+                Industry Leading Audio-Video Solutions.
+                </h2>
+                <p>
+                In a laoreet purus. Integer turpis quam, laoreet id orci nec, ultrices lacinia nunc. Aliquam erat volutpat. Curabitur fringilla in purus eget egestas. Etiam quis.
+                </p>
+                <a class="btn btn-white-outline" href="#">
+                    learn more
+                </a>
+            </div>
+        </div>
+      </div>
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+    </div>
+</section>
+<!-- End Hero Slider -->
+
+<!-- Start Features -->
+<section class="features">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="features-wrapper">
+                    <div class="block">
+                        <span>
+                            <img src="./img/feature-img-1.png" alt="">
+                        </span>
+                        <span>
+                            <h6>
+                                Design
+                            </h6>
+                            <p>
+                            We are providing a professional design based on deeps scientific studies to provide the best and cost-effective solutions for different applications.
+                            </p>
+                        </span>
+                    </div>
+                    <div class="block">
+                        <span>
+                            <img src="./img/feature-img-2.png" alt="">
+                        </span>
+                        <span>
+                            <h6>
+                                Integration
+                            </h6>
+                            <p>
+                            We offer complete AV integration services ranging form conseling and consultation to pinpointing of primary business needs.
+                            </p>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Features -->
+
+<?php include "./inc/solutions.php"?>
+
+<?php include "./inc/projects.php"?>
+
+<?php include "./inc/clients.php"?>
+
+<?php include "./inc/contact.php"?>
+
+<?php include "footer.php"?>
---
+++ projects.php
@@ -0,0 +1,77 @@
+<?php include "header.php"?>
+<?php include "./inc/top-page.php"?>
+
+<!-- Start Page -->
+<div id="projects-page" class="page">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+                <div class="p-item">
+                    <a href="#">
+                        <img src="./img/project-img-1.png" alt="">
+                    </a>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+                <div class="p-item">
+                    <a href="#">
+                        <img src="./img/project-img-2.png" alt="">
+                    </a>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+                <div class="p-item">
+                    <a href="#">
+                        <img src="./img/project-img-3.png" alt="">
+                    </a>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+                <div class="p-item">
+                    <a href="#">
+                        <img src="./img/project-img-4.png" alt="">
+                    </a>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+                <div class="p-item">
+                    <a href="#">
+                        <img src="./img/project-img-5.png" alt="">
+                    </a>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+                <div class="p-item">
+                    <a href="#">
+                        <img src="./img/project-img-5.png" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col">
+                <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-center">
+                    <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                    </a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                    </a>
+                    </li>
+                </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Page -->
+
+<?php include "footer.php"?>
